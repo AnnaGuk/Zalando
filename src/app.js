7881fba7e220d11389d0819ef468b92a7be6281d
Anna Guk <guk.anna1989@gmail.com>
2022-06-04 22:27:42 +0200

    use pure css for hamburger menu

--- src/app.js
+++ src/app.js
@@ -1,29 +1,18 @@
-const hamburgerBtn = document.getElementById("hmbg");
-const mobileMenu = document.getElementById("mbl");
-
-hamburgerBtn.addEventListener("click", function() {
-  if (mobileMenu.style.display === "block") {
-    mobileMenu.style.display = "none";
-  } else {
-    mobileMenu.style.display = "block";
-  }
-});
-
 const button = document.getElementById("button--right");
-button.onclick = function() {
+button.onclick = function () {
   const container = document.getElementById("content");
   sideScroll(container, "right", 25, 100, 10);
 };
 
 const back = document.getElementById("button--left");
-back.onclick = function() {
+back.onclick = function () {
   const container = document.getElementById("content");
   sideScroll(container, "left", 25, 100, 10);
 };
 
 function sideScroll(element, direction, speed, distance, step) {
   scrollAmount = 0;
-  const slideTimer = setInterval(function() {
+  const slideTimer = setInterval(function () {
     if (direction == "left") {
       element.scrollLeft -= step;
     } else {
